d15d8f6a51c010caa37206f118f5e04f8a262290
Sean McAninch <Vinsentwolfgang@gmail.com>
2016-06-03 09:29:08 +0000

    new Updated main page

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 
 
 //--------------------
-//window.onload = resizeRootEm;
-//window.onresize = resizeRootEm;
+window.onload = resizeRootEm;
+window.onresize = resizeRootEm;
 //----------------------
 function resizeRootEm(){
     document.documentElement.style.fontSize = 10 + window.innerWidth/100   + "px";
